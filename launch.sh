e2a81b44ac0cf76a20fdc04d4130f22739779826
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-17 16:19:46 -0500

    Update Launch script

--- launch.sh
+++ launch.sh
@@ -4,6 +4,8 @@ set -x
 
 cd /data
 
+
+
 if ! [[ "$EULA" = "false" ]]; then
   echo "eula=true" > eula.txt
 else
@@ -13,8 +15,9 @@ fi
 
 if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
   rm -fr rm -fr config defaultconfigs kubejs scripts mods packmenu Simple.zip forge*
-  # TODO: Automatically get FileId and FileName to make it generic
-  curl -Lo "Server-Files-$PROJECT_VERSION.zip" "https://edge.forgecdn.net/files/5113/792/Craft%20to%20Exile%202%20SERVER-$PROJECT_VERSION.zip" || exit 9
+  fileName=$(jq -r '.fileName' server-file-info.json)
+  serverPackFileId=$(jq -r '.serverPackFileId' server-file-info.json)
+  curl -Lo "Server-Files-$PROJECT_VERSION.zip" "https://edge.forgecdn.net/files/${serverPackFileId:0:4}/${serverPackFileId:4}/${fileName// /%20}" || exit 9
   unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /data
   FORGE_INSTALLER=$(find . -name "forge-*-installer.jar")
   java -jar "${FORGE_INSTALLER}" --installServer
