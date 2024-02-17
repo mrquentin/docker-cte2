cce58dfa7befb8f9c3f6135a757cfbcf8a8e79b9
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-17 17:07:31 -0500

    Fix script

--- launch.sh
+++ launch.sh
@@ -17,7 +17,8 @@ if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
   rm -fr rm -fr config defaultconfigs kubejs scripts mods packmenu Simple.zip forge*
   fileName=$(jq -r '.fileName' /server-file-info.json)
   serverPackFileId=$(jq -r '.serverPackFileId' /server-file-info.json)
-  curl -Lo "Server-Files-$PROJECT_VERSION.zip" "https://edge.forgecdn.net/files/${serverPackFileId:0:4}/${serverPackFileId:4}/${fileName// /%20}" || exit 9
+  serverFileName="${fileName%-*} SERVER-${fileName##*-}"
+  curl -Lo "Server-Files-$PROJECT_VERSION.zip" "https://edge.forgecdn.net/files/${serverPackFileId:0:4}/${serverPackFileId:4}/${serverFileName// /%20}" || exit 9
   unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /data
   FORGE_INSTALLER=$(find . -name "forge-*-installer.jar")
   java -jar "${FORGE_INSTALLER}" --installServer
